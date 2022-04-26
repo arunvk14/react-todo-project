58f5edd379e595f8c9750c67beb344a641407ec3
arun <arun.vadakkekara@waypoint-systems.com>
2022-04-26 17:11:31 +0530

    Updates

---
+++ precache-manifest.52244d4be0237b0b14229930bebbdc94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68bcaf67022e9508d9ba0efde4c79ea1",
+    "url": "/react-todo-project/index.html"
+  },
+  {
+    "revision": "3e4ef5732ba843f089c1",
+    "url": "/react-todo-project/static/css/main.b6ff71cb.chunk.css"
+  },
+  {
+    "revision": "92a07cfc0076cd9b6880",
+    "url": "/react-todo-project/static/js/2.dea53039.chunk.js"
+  },
+  {
+    "revision": "3e4ef5732ba843f089c1",
+    "url": "/react-todo-project/static/js/main.ba0d3f53.chunk.js"
+  },
+  {
+    "revision": "cefc1f55c629f196fd6a",
+    "url": "/react-todo-project/static/js/runtime~main.eb2bdb26.js"
+  }
+]);
