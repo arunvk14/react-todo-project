93d41c3a857a04fd8364a9cab69a2027f68a3090
arun <arun.vadakkekara@waypoint-systems.com>
2022-04-27 15:33:10 +0530

    Updates

--- precache-manifest.62b7a175a8ae7ec8bab44c23ecc47484.js
+++ precache-manifest.62b7a175a8ae7ec8bab44c23ecc47484.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "01fd5712723017f5724db8b290655552",
+    "revision": "d2460f6c37fcace3dcb9366bdf866d33",
     "url": "/react-todo-project/index.html"
   },
   {
-    "revision": "f33103a5733741f34097",
-    "url": "/react-todo-project/static/css/main.b6ff71cb.chunk.css"
+    "revision": "8fad68415b743cc42b86",
+    "url": "/react-todo-project/static/css/main.cd4048eb.chunk.css"
   },
   {
     "revision": "d89cf039863859598e34",
     "url": "/react-todo-project/static/js/2.284edbf6.chunk.js"
   },
   {
-    "revision": "f33103a5733741f34097",
-    "url": "/react-todo-project/static/js/main.1945ae9a.chunk.js"
+    "revision": "8fad68415b743cc42b86",
+    "url": "/react-todo-project/static/js/main.1152d589.chunk.js"
   },
   {
     "revision": "cefc1f55c629f196fd6a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-todo-project/precache-manifest.5a20de28096688c8d3b42d4483f790a9.js"
+  "/react-todo-project/precache-manifest.62b7a175a8ae7ec8bab44c23ecc47484.js"
 );
 
 self.addEventListener('message', (event) => {
